fa173f6fb05a1ad8a52f554aa088b44781425879
Steven Yuen Fay Tsang <steven.tsang@dumont.de>
2019-02-19 08:50:32 +0100

    added python3 to base install

--- http/install.sh
+++ http/install.sh
@@ -25,7 +25,7 @@ mkswap "${device}1"
 mkfs.btrfs -L "rootfs" "${device}2"
 mount "${device}2" /mnt
 
-pacstrap /mnt base grub openssh sudo polkit btrfs-progs haveged
+pacstrap /mnt base grub openssh sudo polkit btrfs-progs haveged python
 swapon "${device}1"
 genfstab -p /mnt >> /mnt/etc/fstab
 swapoff "${device}1"
